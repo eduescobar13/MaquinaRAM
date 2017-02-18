dd7068ab305c071816c23d7a8f13a9e617801742
eduescobar13 <eduescal13@gmail.com>
2017-02-18 16:35:37 +0000

    Añadida la primera versión del fichero principal

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//|-----------------------------------------------------------------------|
+//| Author   : EDUARDO ESCOBAR ALBERTO                                    |
+//| Date     : 21/02/2017                                                 |
+//| Comments : Diseño y Análisis de Algoritmos                            |
+//|            Escuela Técnica Superior de Ingeniería Informática         |
+//|            Universidad de La Laguna                                   |  
+//|                                                                       |
+//| Compiling:                                                            |                                                         
+//|-----------------------------------------------------------------------|
+
+#include <iostream>
+#include "UnidadEntrada.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) { // -----> FUNCIÓN PRINCIPAL.
+
+	system ("clear");
+
+	UnidadEntrada *UE;
+
+	cout << endl;
+	cout << "  ___|PRÁCTICA 1: DISEÑO Y ANÁLISIS DE ALGORITMOS|___" << endl;
+	cout << "---------------------- MÁQUINA RAM ----------------------" << endl;
+
+	UE = new UnidadEntrada(argv[1]); // Creación de un objeto de unidad de entrada pasando el nombre del fichero (línea de comandos).
+
+	return (0); 
+
+}
